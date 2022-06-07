47c6cccec0297d264dddd89304d654bcbe89bc94
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-06-07 17:24:10 -0500

    03-bool-condicion.dart

---
+++ 03-bool-condicion.dart
@@ -0,0 +1,13 @@
+void main() {
+  
+  
+  bool? isActive;
+  //se entiende que es null
+  if ( isActive == null ) {
+    print( 'isActive es null' );
+  } else {
+    print( 'No es null' );
+  }
+  
+  
+}
