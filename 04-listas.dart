96984c5fd3c75993606a608546fdda64031adeaf
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-06-07 17:46:21 -0500

    04-listas.dart

---
+++ 04-listas.dart
@@ -0,0 +1,13 @@
+void main() {
+  
+  
+  List<int> numeros = [1,2,3,4,5,6,7,8,9,10];
+  numeros.add(11);
+  print( numeros );
+  
+  //obtener la posicion
+  print(numeros[0]);
+  final masNumeros = List.generate(100, (int index) => index );
+ 
+  print(masNumeros);
+}
