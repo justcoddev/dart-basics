aaefea1c580dc1d0e572b79ef02e5ebb1529872b
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-06-07 17:13:24 -0500

    02-tipos-numero-string.dart

---
+++ 02-tipos-numero-string.dart
@@ -0,0 +1,18 @@
+
+void main()  {
+  
+  // Strings 
+  final String nombre = 'Teddy';
+  final apellido = 'Summers';
+  
+//   nombre = 'Cristina';
+  
+  print('$nombre $apellido');
+  
+  
+//   Números
+  int empleados = 10;
+  double salario = 1856.25;
+  
+  print( empleados );
+  print( salario );
