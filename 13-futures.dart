37d55c58cc3c9e2305ee9f1bdb6a2755097ed709
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-06-08 00:31:00 -0500

    13-futures.dart

---
+++ 13-futures.dart
@@ -0,0 +1,30 @@
+//aquí no
+void main() {
+  
+  print('Antes de la petición');
+  
+  httpGet('https://api.nasa.com/aliens')
+     .then( (data) {
+       
+       print( data.toUpperCase() );
+       
+     });
+  
+  
+  print('Fin del programa');
+    
+}
+
+
+
+
+Future<String> httpGet( String url ) {
+  return Future.delayed( 
+    Duration( seconds: 3 ), () =>'Hola Mundo - 3 segundos' 
+  );
+}
+
+
+
+
+
